1dd36a326cca095c9ae54d365c328326a122925f
Ishita Talwar <103492839+ishitatalwar@users.noreply.github.com>
2026-09-19 17:35:36 +0530

    Time: 0 ms (100%), Space: 42.3 MB (5.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -16,17 +16,42 @@ public:
         // }
         // return -1;
 
+        // int n = nums.size();
+        // unordered_map<int, int> mp;
+        // for(int i = 0; i<n; i++){
+        //     mp[nums[i]]++;
+        // }
+        // for(auto it : mp){
+        //     if(it.second > (n/2)){
+        //         return it.first;
+        //     }
+        // }
+        // return -1;
+        // optimal approach
         int n = nums.size();
-        unordered_map<int, int> mp;
+        int cnt = 0;
+        int el;
         for(int i = 0; i<n; i++){
-            mp[nums[i]]++;
-        }
-        for(auto it : mp){
-            if(it.second > (n/2)){
-                return it.first;
+            if(cnt == 0){
+                el = nums[i];
+                cnt = 1;
+            }
+            else if(el == nums[i]){
+                cnt++;
+            }
+            else{
+                cnt--;
             }
         }
+        int cnt1 = 0;
+        for(int i = 0; i<n; i++){
+            if(nums[i] == el){
+                cnt1++;
+            }
+        }
+        if(cnt1>(n/2)){
+            return el;
+        }
         return -1;
-        
     }
 };
